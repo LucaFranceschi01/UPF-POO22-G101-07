2f5283e45f322c14c00546e877358110ba76c105
LucaFranceschi01 <luca.franceschi01@estudiant.upf.edu>
2022-11-09 12:08:22 +0100

    wrong characters not fixed

--- Lab3/Association/src/Delegate.java
+++ Lab3/Association/src/Delegate.java
@@ -37,17 +37,19 @@ public class Delegate extends Member {
         if(d.getHeadOf() != null) {
             text = text.concat("\\n - ").concat(getName());
         }
-        Image image = new Image("Lab3/Association/img/del-".concat(String.valueOf(d.getPhone())).concat(".jpg"), 600, 600);
+        Image image = new Image("Lab3/Association/img/del-".concat(String.valueOf(d.getPhone())).concat(".png"), 600, 600);
         image.setBitMatrix(QRLib.generateQRCodeImage(text, 600, 600));
         image.getBitmap(); // to set bitmap
+        image.save();
         return image;
     }
 
     public Image genRegularQR(Regular r) {
         String text = "This is a QR for a Regular Member. Climate change doesn\'t matter, if you stay indoors.";
-        Image image = new Image("Lab3/Association/img/reg-".concat(String.valueOf(r.getPhone())).concat(".jpg"), 600, 600);
+        Image image = new Image("Lab3/Association/img/reg-".concat(String.valueOf(r.getPhone())).concat(".png"), 600, 600);
         image.setBitMatrix(QRLib.generateQRCodeImage(text, 600, 600));
         image.getBitmap(); // to set bitmap
+        image.save();
         return image;
     }
 
--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # UPF-POO22-G101-07
 ## OOP Repository of Luca Franceschi & Alejandro González for Lab deliveries 
 
-return head delegates of every headq DONE
-generate qrs gendelegateqr genregularqr
-signup new members signupdelegate signupregular
-texts for qrs
-print all members of organization DONE
+characters not read properly accented QRs
 actions, infoaction
 delegate proposenewactions and signupaction
 testear cosas
